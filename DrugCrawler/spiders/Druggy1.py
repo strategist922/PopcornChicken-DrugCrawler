e1286dd243a2316a8cc73b42551c36babe33fcf4
Kunimahooo <liethan080@gmail.com>
2019-02-20 15:05:42 -0800

    Success downloading all A diseases

--- DrugCrawler/spiders/Druggy1.py
+++ DrugCrawler/spiders/Druggy1.py
@@ -4,16 +4,16 @@ import scrapy
 
 class Druggy1(scrapy.Spider):
     name = 'Druggy1'
-    start_urls = ['https://rarediseases.org/for-patients-and-families/information-resources/rare-disease-information/']
+    start_urls = ['https://rarediseases.org/?s=A&post_type=rare-diseases']
 
     def parse(self, response):
-        SET_SELECTOR = 'body.rare-diseases-template-default'
+        SET_SELECTOR = 'h3.rdr-one-title'
         for brickset in response.css(SET_SELECTOR):
 
             TITLE_SELECTOR = 'h3 a ::text'
            # PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
             yield {
-                'title': brickset.css(TITLE_SELECTOR).extract_first(),
+                'title': brickset.css(TITLE_SELECTOR).extract(),
                # 'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
             }
 
