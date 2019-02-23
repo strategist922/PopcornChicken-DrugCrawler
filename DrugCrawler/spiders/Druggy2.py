c5d097398116b6aa686734572e6b9622b391ef05
Kunimahooo <liethan080@gmail.com>
2019-02-22 21:16:11 -0800

    Common Names

--- DrugCrawler/spiders/Druggy2.py
+++ DrugCrawler/spiders/Druggy2.py
@@ -3,7 +3,7 @@ import scrapy
 
 class Druggy1(scrapy.Spider):
     name = 'Druggy2'
-    start_urls = ['https://rarediseases.info.nih.gov/diseases/browse-by-first-letter/A']
+    start_urls = ['https://rarediseases.info.nih.gov/diseases/browse-by-first-letter/Z']
 
     # custom_settings = {
     #   'DEPTH_LIMIT': 1
@@ -15,16 +15,10 @@ class Druggy1(scrapy.Spider):
         SET_SELECTOR = 'ul.listing-diseases'
         for brickset in response.css(SET_SELECTOR):
 
-            SCIENTIFIC_NAME = 'ul span ::text'
-            COMMON_NAME = 'ul a::text'
-            y = 0
-            z = int(len(brickset.css(COMMON_NAME).extract())) - 1
-            while y <= z:
-                x = " (" + str(brickset.css(SCIENTIFIC_NAME).extract()[y]) + "), "
-                y += 1
-           # PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            # SCIENTIFIC_NAME = 'ul span ::text'
+            COMMON_NAME = 'ul a ::text'
             yield {
-                'Diseases' : x.join(brickset.css(COMMON_NAME).extract())
+                'Common Names Z': brickset.css(COMMON_NAME).extract()
                # 'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
             }
         NEXT_PAGE_SELECTOR = 'ul.m10.clearfix.alpha-search.alpha-block-list a ::attr(href)'
